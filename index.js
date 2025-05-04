d62d6b35e92873dbd47793260b255d90cc27c1ae
Annette-2025 <limogesa@outlook.com>
2025-05-04 15:48:48 -0400

    Revert "visual updates, replaced javascript alerts with input fields or outpu…"

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-const port = 1001
 
 http.createServer((req, res)=>{
     var q = url.parse(req.url,true);
@@ -18,8 +17,8 @@ http.createServer((req, res)=>{
     //res.writeHead(200,{'Content-Type': 'text/html'});
     //var readStream = fs.createReadStream('public/plantList.html');
     //readStream.pipe(res);
-}).listen(port);
-console.log("server running on port " + port + "\nAccess Webpage here: " + "http://localhost:" + port + "/pages/calendar.html");
+}).listen(1001);
+console.log("server running on port 1001");
 
 
 
